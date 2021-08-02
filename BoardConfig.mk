78a26c68ccf0731eb20b2f6c2bc50271e4e1c3a2
Erfan Abdi <erfangplus@gmail.com>
2021-08-02 20:39:51 +0430

    GS290: Add device and BoardConfig.mk

---
+++ BoardConfig.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from gigaset mt6763-common
+-include device/gigaset/mt6763-common/BoardConfigCommon.mk
+
+# Assertions
+TARGET_OTA_ASSERT_DEVICE += GS290,yggdrasil
+
+# Display
+TARGET_SCREEN_DENSITY := 420
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/gigaset/mt6763
+TARGET_KERNEL_CONFIG := gs290_defconfig
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
+# inherit from the proprietary version
+-include vendor/gigaset/GS290-unified/BoardConfigVendor.mk
